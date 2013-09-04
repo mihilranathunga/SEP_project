3f77d2520db0e32d689fdfbf1d9e78b5f72e8e30
Mihil Ranathunga <mihilranathunga@gmail.com>
2013-09-04 11:20:31 +0530

    First initiation commit

---
+++ gen/cse/sep/mihil/bridgemanagementhelper/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cse.sep.mihil.bridgemanagementhelper;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
