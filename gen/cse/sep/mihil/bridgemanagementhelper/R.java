1b90f12fb08a83f7d292a5028da70d1d5684fa8d
Mihil Ranathunga <mihilranathunga@gmail.com>
2013-09-05 00:27:16 +0530

    camera functionality merged

--- gen/cse/sep/mihil/bridgemanagementhelper/R.java
+++ gen/cse/sep/mihil/bridgemanagementhelper/R.java
@@ -21,10 +21,16 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_icon=0x7f020000;
+        public static final int existing_images=0x7f020001;
+        public static final int from_database=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int button1=0x7f080001;
+        public static final int button2=0x7f080002;
+        public static final int new_project_button=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,8 +40,12 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int album_name=0x7f050006;
         public static final int app_name=0x7f050000;
+        public static final int existing_images=0x7f050004;
+        public static final int from_the_database=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int new_project_button=0x7f050003;
     }
     public static final class style {
         /** 
