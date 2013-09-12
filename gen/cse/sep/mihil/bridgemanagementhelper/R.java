6bb8b82394230d1470272432a22975eeab7ba20e
Mihil Ranathunga <mihilranathunga@gmail.com>
2013-09-13 02:25:19 +0530

    Image Addded to annotate

--- gen/cse/sep/mihil/bridgemanagementhelper/R.java
+++ gen/cse/sep/mihil/bridgemanagementhelper/R.java
@@ -10,6 +10,9 @@ package cse.sep.mihil.bridgemanagementhelper;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int opaque_black=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,8 +20,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int button_icon=0x7f020000;
@@ -27,25 +30,34 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080002;
-        public static final int new_project_button=0x7f080000;
+        public static final int action_settings=0x7f090006;
+        public static final int button1=0x7f090004;
+        public static final int button2=0x7f090005;
+        public static final int confirm=0x7f090002;
+        public static final int enter_project_name=0x7f090001;
+        public static final int new_project_button=0x7f090003;
+        public static final int taken_image=0x7f090000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_annotation=0x7f030000;
+        public static final int activity_entername=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int album_name=0x7f050006;
-        public static final int app_name=0x7f050000;
-        public static final int existing_images=0x7f050004;
-        public static final int from_the_database=0x7f050005;
-        public static final int hello_world=0x7f050002;
-        public static final int new_project_button=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int album_name=0x7f060007;
+        public static final int annotate=0x7f060009;
+        public static final int app_name=0x7f060000;
+        public static final int cancel=0x7f060003;
+        public static final int cannot=0x7f06000a;
+        public static final int confirm=0x7f060002;
+        public static final int enter_project_name=0x7f060008;
+        public static final int existing_images=0x7f060005;
+        public static final int from_the_database=0x7f060006;
+        public static final int new_project_button=0x7f060004;
     }
     public static final class style {
         /** 
@@ -69,10 +81,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int Theme_Transparent=0x7f070002;
     }
 }
