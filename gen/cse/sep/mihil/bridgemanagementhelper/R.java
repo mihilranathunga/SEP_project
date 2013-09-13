73bc9d67c72dfe0523df173def37c4b6f009b855
Mihil Ranathunga <mihilranathunga@gmail.com>
2013-09-13 09:54:22 +0530

    Added visuall cue an item label use case

--- gen/cse/sep/mihil/bridgemanagementhelper/R.java
+++ gen/cse/sep/mihil/bridgemanagementhelper/R.java
@@ -8,6 +8,9 @@
 package cse.sep.mihil.bridgemanagementhelper;
 
 public final class R {
+    public static final class array {
+        public static final int selection_array=0x7f070000;
+    }
     public static final class attr {
     }
     public static final class color {
@@ -30,21 +33,24 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
-        public static final int button1=0x7f090004;
-        public static final int button2=0x7f090005;
-        public static final int confirm=0x7f090002;
-        public static final int enter_project_name=0x7f090001;
-        public static final int new_project_button=0x7f090003;
-        public static final int taken_image=0x7f090000;
+        public static final int action_settings=0x7f0a0008;
+        public static final int button1=0x7f0a0005;
+        public static final int button2=0x7f0a0006;
+        public static final int confirm=0x7f0a0003;
+        public static final int enter_project_name=0x7f0a0002;
+        public static final int new_project_button=0x7f0a0004;
+        public static final int selection_spinner=0x7f0a0000;
+        public static final int spinnerTarget=0x7f0a0007;
+        public static final int taken_image=0x7f0a0001;
     }
     public static final class layout {
         public static final int activity_annotation=0x7f030000;
         public static final int activity_entername=0x7f030001;
         public static final int activity_main=0x7f030002;
+        public static final int spinner_layout=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
@@ -81,11 +87,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int Theme_Transparent=0x7f070002;
+        public static final int AppTheme=0x7f080001;
+        public static final int Theme_Transparent=0x7f080002;
     }
 }
--- src/cse/sep/mihil/bridgemanagementhelper/AnnotationActivity.java
+++ src/cse/sep/mihil/bridgemanagementhelper/AnnotationActivity.java
@@ -9,7 +9,9 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class AnnotationActivity extends Activity {
 
@@ -28,6 +30,15 @@ public class AnnotationActivity extends Activity {
 		projectName = getIntent().getStringExtra("PROJECT_NAME");
 		
 		handleBigCameraPhoto();
+		
+		Spinner spinner = (Spinner) findViewById(R.id.selection_spinner);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.selection_array, R.layout.spinner_layout);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
 		
 		
 	}
